87bff7fe468012a977c4c39622d174ff06e0274d
Tobias Schramm <tobleminer@gmail.com>
2018-02-13 11:39:03 +0100

    [libmeshneighbour] (fixup) fix build

--- libs/libmeshneighbour/src/libmeshneighbour.h
+++ libs/libmeshneighbour/src/libmeshneighbour.h
@@ -16,9 +16,9 @@ struct mesh_neighbour_ctx {
 	struct list_head interfaces;
 };
 
-int mesh_get_neighbours(struct neighbour_ctx *neigh_ctx);
+int mesh_get_neighbours(struct mesh_neighbour_ctx *neigh_ctx);
 int mesh_get_neighbours_interfaces(struct list_head *interfaces, struct list_head* neighbours);
-int mesh_get_neighbours_ubus(struct ubus_context *ubus_ctx, struct neighbour_ctx *neigh_ctx);
+int mesh_get_neighbours_ubus(struct ubus_context *ubus_ctx, struct mesh_neighbour_ctx *neigh_ctx);
 
 void mesh_free_neighbours(struct list_head *neighbours);
 void mesh_free_neighbours_ctx(struct mesh_neighbour_ctx *ctx);
