7ef940de548c7e716bd84684e039dd2cfb38cc25
Tobias Schramm <tobleminer@gmail.com>
2018-02-14 13:01:36 +0100

    Add librespondd support

--- libs/libmeshneighbour/src/libmeshneighbour.h
+++ libs/libmeshneighbour/src/libmeshneighbour.h
@@ -2,11 +2,13 @@
 #define _LIBMESHNEIGHBOUR_H_
 
 #include <libgluonutil.h>
+#include <librespondd.h>
 #include <libubox/list.h>
 
 struct mesh_neighbour {
-	unsigned char hwaddr[6];
+	struct in6_addr addr;
 	struct gluonutil_interface *iface;
+	void *priv;
 
 	struct list_head list;
 };
@@ -16,11 +18,10 @@ struct mesh_neighbour_ctx {
 	struct list_head interfaces;
 };
 
-int mesh_get_neighbours(struct mesh_neighbour_ctx *neigh_ctx);
-int mesh_get_neighbours_interfaces(struct list_head *interfaces, struct list_head* neighbours);
-int mesh_get_neighbours_ubus(struct ubus_context *ubus_ctx, struct mesh_neighbour_ctx *neigh_ctx);
+typedef int (*neighbour_cb)(char* json_data, size_t data_len, struct librespondd_pkt_info *pktinfo, struct mesh_neighbour *neigh, void* priv);
 
-void mesh_free_neighbours(struct list_head *neighbours);
-void mesh_free_neighbours_ctx(struct mesh_neighbour_ctx *ctx);
+int mesh_get_neighbours_respondd(struct mesh_neighbour_ctx *neigh_ctx, unsigned short respondd_port, neighbour_cb cb, void *priv);
+
+void mesh_free_respondd_neighbours_ctx(struct mesh_neighbour_ctx *ctx);
 
 #endif
