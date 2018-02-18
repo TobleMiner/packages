f728dc7c81de4022cc58ef54b29ff740977b32e4
Tobias Schramm <tobleminer@gmail.com>
2018-02-18 10:06:15 +0100

    Remove duplicate nodeids

--- libs/libmeshneighbour/src/libmeshneighbour.c
+++ libs/libmeshneighbour/src/libmeshneighbour.c
@@ -29,10 +29,6 @@
 #include <errno.h>
 #include <stdbool.h>
 
-#include <libmnl/libmnl.h>
-#include <linux/if_link.h>
-#include <linux/rtnetlink.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/time.h>
@@ -44,192 +40,11 @@
 #include <libgluonutil.h>
 #include <libubox/list.h>
 #include <libubus.h>
+#include <json-c/json.h>
 
 #include "libmeshneighbour.h"
 
 #define IPV6_MCAST_ALL_NODES (struct in6_addr){ .s6_addr = { 0xff, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 } }
-
-
-#ifndef container_of
-#define container_of(ptr, type, member)					\
-	({								\
-		const typeof(((type *) NULL)->member) *__mptr = (ptr);	\
-		(type *) ((char *) __mptr - offsetof(type, member));	\
-	})
-#endif
-
-/*
-void mesh_free_neighbours(struct list_head *neighbours) {
-	struct mesh_neighbour *neigh, *next;
-	list_for_each_entry_safe(neigh, next, neighbours, list) {
-		list_del(&neigh->list);
-		free(neigh);
-	}
-}
-
-void mesh_free_neighbours_ctx(struct mesh_neighbour_ctx *ctx) {
-	mesh_free_neighbours(&ctx->neighbours);
-	gluonutil_free_interfaces(&ctx->interfaces);
-}
-
-struct mesh_neigh_ptr_ctx {
-	struct list_head *interfaces;
-	struct list_head *neighbours;
-};
-
-static struct gluonutil_interface *find_interface_ifindex(struct list_head* interfaces, unsigned int ifindex) {
-	struct gluonutil_interface *iface;
-	list_for_each_entry(iface, interfaces, list) {
-		if(iface->ifindex == ifindex) {
-			return iface;
-		}
-	}
-	return NULL;
-}
-
-struct mesh_interface_ctx {
-	struct list_head *neighbours;
-	struct gluonutil_interface *iface;
-};
-
-#define member_size(type, member) sizeof(((type *)0)->member)
-
-static int neigh_attr_cb(const struct nlattr *attr, void *data) {
-	int type = mnl_attr_get_type(attr);
-	if(mnl_attr_type_valid(attr, NDA_MAX) < 0) {
-		goto out;
-	}
-
-	if(type == NDA_DST) {
-		struct mesh_interface_ctx *ctx = data;
-		struct mesh_neighbour *neigh = malloc(sizeof(struct mesh_neighbour));
-		if(!neigh) {
-			goto out;
-		}
-		neigh->iface = ctx->iface;
-		struct in6_addr *addr = mnl_attr_get_payload(attr);
-		memcpy(&neigh->addr, addr, sizeof(*addr));
-		list_add(&neigh->list, ctx->neighbours);
-		char buf[INET6_ADDRSTRLEN];
-		printf("Added neighbour %s ", inet_ntop(AF_INET6, addr, buf, sizeof(buf)));
-	}
-
-out:
-	return MNL_CB_OK;
-}
-
-static int neigh_cb(const struct nlmsghdr *nlh, void *data)
-{
-	struct ndmsg* ndmsg = mnl_nlmsg_get_payload(nlh);
-	switch(ndmsg->ndm_state) {
-		case NUD_INCOMPLETE:
-		case NUD_FAILED:
-			return MNL_CB_OK;
-	}
-
-	struct mesh_neigh_ptr_ctx *ctx = data;
-	struct gluonutil_interface *iface = find_interface_ifindex(ctx->interfaces, ndmsg->ndm_ifindex);
-	if(!iface) {
-		return MNL_CB_OK;
-	}
-
-	printf("Interface %d, state %x\n", ndmsg->ndm_ifindex, ndmsg->ndm_state);
-
-	struct mesh_interface_ctx iface_ctx;
-	iface_ctx.neighbours = ctx->neighbours;
-	iface_ctx.iface = iface;
-	mnl_attr_parse(nlh, sizeof(*ndmsg), neigh_attr_cb, &iface_ctx);
-	return MNL_CB_OK;
-}
-
-int mesh_get_neighbours_interfaces(struct list_head *interfaces, struct list_head* neighbours) {
-	int err;
-	struct mnl_socket *nl;
-	char buf[MNL_SOCKET_BUFFER_SIZE];
-	struct nlmsghdr *nlh;
-	struct ndmsg *ndm;
-	unsigned int seq, portid;
-
-	nlh = mnl_nlmsg_put_header(buf);
-	nlh->nlmsg_type = RTM_GETNEIGH;
-	nlh->nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
-	nlh->nlmsg_seq = seq = time(NULL);
-	ndm = mnl_nlmsg_put_extra_header(nlh, sizeof(struct ndmsg));
-
-	ndm->ndm_family = AF_INET6;
-
-	nl = mnl_socket_open(NETLINK_ROUTE);
-	if(!nl) {
-		err = -errno;
-		goto fail;
-	}
-
-	if(mnl_socket_bind(nl, 0, MNL_SOCKET_AUTOPID) < 0) {
-		err = -errno;
-		goto fail_nl;
-	}
-	portid = mnl_socket_get_portid(nl);
-
-	if(mnl_socket_sendto(nl, nlh, nlh->nlmsg_len) < 0) {
-		err = -errno;
-		goto fail_nl;
-	}
-
-	struct mesh_neigh_ptr_ctx ctx;
-	ctx.interfaces = interfaces;
-	ctx.neighbours = neighbours;
-
-	ssize_t len = 0;
-	int ret = 0;
-	while((len = mnl_socket_recvfrom(nl, buf, sizeof(buf))) > 0) {
-		ret = mnl_cb_run(buf, len, seq, portid, neigh_cb, &ctx);
-		if(ret != MNL_CB_OK) {
-			break;
-		}
-	}
-
-	// TODO filter local mac addresses
-
-	return 0;
-
-fail_nl:
-	mnl_socket_close(nl);
-fail:
-	return err;
-}
-
-int mesh_get_neighbours_ubus(struct ubus_context *ubus_ctx, struct mesh_neighbour_ctx *neigh_ctx) {
-	int err = get_neighbours_common(ubus_ctx, neigh_ctx);
-	if(err) {
-		goto fail;
-	}
-
-	err = mesh_get_neighbours_interfaces(&neigh_ctx->interfaces, &neigh_ctx->neighbours);
-	if(err) {
-		goto fail_interfaces;
-	}
-
-	return 0;
-
-fail_interfaces:
-	gluonutil_free_interfaces(&neigh_ctx->interfaces);
-fail:
-	return err;
-}
-
-int mesh_get_neighbours(struct mesh_neighbour_ctx *neigh_ctx) {
-	struct ubus_context *ubus_ctx = ubus_connect(NULL);
-	if(!ubus_ctx) {
-		return -ECONNREFUSED;
-	}
-
-	int err = mesh_get_neighbours_ubus(ubus_ctx, neigh_ctx);
-
-	ubus_free(ubus_ctx);
-
-	return err;
-}
-*/
 
 static int get_neighbours_common(struct ubus_context *ubus_ctx, struct mesh_neighbour_ctx *neigh_ctx) {
 	neigh_ctx->neighbours = (struct list_head)LIST_HEAD_INIT(neigh_ctx->neighbours);
@@ -238,11 +53,18 @@ static int get_neighbours_common(struct ubus_context *ubus_ctx, struct mesh_neig
 	return gluonutil_get_mesh_interfaces(ubus_ctx, &neigh_ctx->interfaces);
 }
 
+static void mesh_free_respondd_neighbour(struct mesh_neighbour *neigh) {
+	if(neigh->nodeid) {
+		free(neigh->nodeid);
+	}
+	free(neigh);
+}
+
 void mesh_free_respondd_neighbours(struct list_head *neighbours) {
 	struct mesh_neighbour *neigh, *next;
 	list_for_each_entry_safe(neigh, next, neighbours, list) {
 		list_del(&neigh->list);
-		free(neigh);
+		mesh_free_respondd_neighbour(neigh);
 	}
 }
 
@@ -258,7 +80,17 @@ struct mesh_respondd_ctx {
 	neighbour_cb cb;
 };
 
-static int mesh_respondd_cb(char* json_data, size_t data_len, struct librespondd_pkt_info *pktinfo, void* priv) {
+static struct mesh_neighbour *find_neighbour_nodeid(const char *nodeid, const struct list_head *neighbours) {
+	struct mesh_neighbour *neighbour;
+	list_for_each_entry(neighbour, neighbours, list) {
+		if(!strcmp(nodeid, neighbour->nodeid)) {
+			return neighbour;
+		}
+	}
+	return NULL;
+}
+
+static int mesh_respondd_cb(const char *json_data, size_t data_len, const struct librespondd_pkt_info *pktinfo, void *priv) {
 	// pktinfo not set, something is not right
 	if(!pktinfo->ifindex) {
 		goto out;
@@ -266,17 +98,42 @@ static int mesh_respondd_cb(char* json_data, size_t data_len, struct librespondd
 
 	struct mesh_respondd_ctx *ctx = priv;
 
+	struct json_object *json_root = json_tokener_parse(json_data);
+	if(!json_root) {
+		goto out;
+	}
+	
+	struct json_object *json_nodeid;
+	if(!json_object_object_get_ex(json_root, "node_id", &json_nodeid)) {
+		goto out_json;
+	}
+
+	const char *nodeid = json_object_get_string(json_nodeid);
+	if(!nodeid) {
+		goto out_json;
+	}
+
+	if(find_neighbour_nodeid(nodeid, ctx->neighbours)) {
+		goto out_json;
+	}
+
+
 	struct mesh_neighbour *neighbour = malloc(sizeof(struct mesh_neighbour));
 	if(!neighbour) {
 		goto out;
 	}
 	memset(neighbour, 0, sizeof(*neighbour));
 
+	neighbour->nodeid = strdup(nodeid);
+	if(!neighbour->nodeid) {
+		goto out_neighbour;
+	}
+
 	neighbour->iface = ctx->iface;
 	neighbour->addr = pktinfo->src_addr;
 
 	if(ctx->cb) {
-		if(ctx->cb(json_data, data_len, pktinfo, neighbour, ctx->cb_priv)) {
+		if(ctx->cb(json_root, pktinfo, neighbour, ctx->cb_priv)) {
 			goto out_neighbour;
 		}
 	}
@@ -286,12 +143,14 @@ static int mesh_respondd_cb(char* json_data, size_t data_len, struct librespondd
 	return RESPONDD_CB_OK;
 
 out_neighbour:
-	free(neighbour);
+	mesh_free_respondd_neighbour(neighbour);
+out_json:
+	json_object_put(json_root);
 out:
 	return RESPONDD_CB_OK;
 }
 
-int mesh_get_neighbours_respondd_interfaces(struct list_head *interfaces, struct list_head* neighbours, unsigned short respondd_port, neighbour_cb cb, void *priv) {
+static int mesh_get_neighbours_respondd_interfaces(struct list_head *interfaces, struct list_head* neighbours, unsigned short respondd_port, neighbour_cb cb, void *priv) {
 	struct gluonutil_interface *iface;
 	struct sockaddr_in6 sock_addr;
 	sock_addr.sin6_family = AF_INET6;
@@ -309,9 +168,11 @@ int mesh_get_neighbours_respondd_interfaces(struct list_head *interfaces, struct
 
 		sock_addr.sin6_scope_id = iface->ifindex;
 
+		LIST_HEAD(neighbours_if);
+
 		struct mesh_respondd_ctx ctx = {
 			.iface = iface,
-			.neighbours = neighbours,
+			.neighbours = &neighbours_if,
 			.cb_priv = priv,
 			.cb = cb,
 		};
@@ -319,12 +180,14 @@ int mesh_get_neighbours_respondd_interfaces(struct list_head *interfaces, struct
 		if(ret) {
 			err = ret;
 		}
+
+		list_splice(&neighbours_if, neighbours);
 	}
 
 	return err;
 }
 
-int mesh_get_neighbours_respondd_ubus(struct ubus_context *ubus_ctx, struct mesh_neighbour_ctx *neigh_ctx, unsigned short respondd_port, neighbour_cb cb, void *priv) {
+static int mesh_get_neighbours_respondd_ubus(struct ubus_context *ubus_ctx, struct mesh_neighbour_ctx *neigh_ctx, unsigned short respondd_port, neighbour_cb cb, void *priv) {
 	int err = get_neighbours_common(ubus_ctx, neigh_ctx);
 	if(err) {
 		goto fail;
